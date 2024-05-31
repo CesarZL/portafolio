6fcb174aa6617f5adb7dae95a1debf8975f5c1d9
cesar zavala <2030241@upv.edu.mx>
2024-05-30 20:17:25 -0600

    navbar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="sticky top-[100vh] py-10 border-t bg-white/80 dark:bg-gray-800/80 border-gray-200 dark:border-gray-700 backdrop-blur-lg">
+<footer class="sticky top-[100vh] py-10 border-t bg-white/80 dark:bg-gray-900/80 border-gray-200 dark:border-gray-700 backdrop-blur-lg">
     <div class="max-w-screen-xl mx-auto px-5">
         <div class="max-w-screen-xl mx-auto px-5">
             <p class="text-center text-sm text-slate-600 dark:text-slate-300 [text-wrap:balance]">
